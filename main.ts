f2b547491a4ccb8213c108065e035941286c4561
minecraftMattis <mattis.lueger@icloud.com>
2024-06-15 11:00:51 +0200

    laermampel

--- main.ts
+++ main.ts
@@ -1,7 +1,48 @@
 let lautstärke = 0
 basic.forever(function () {
     lautstärke = input.soundLevel()
-    if (lautstärke > 100) {
-        music.play(music.stringPlayable("C D E F G A B C5 ", 220), music.PlaybackMode.UntilDone)
+    if (lautstärke > 160) {
+        music.play(music.stringPlayable("C D E F G A B C5 ", 220), music.PlaybackMode.InBackground)
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+    } else if (lautstärke > 120) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+    } else if (lautstärke > 80) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+    } else if (lautstärke > 40) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            # . . . .
+            `)
+    } else if (lautstärke > 10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else {
+        basic.showIcon(IconNames.Heart)
     }
 })
